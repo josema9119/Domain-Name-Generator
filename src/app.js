ef8cbadefa61891eeb7f2a350814b9ec71709574
josema9119 <jose.ma.mm.91@gmail.com>
2022-03-17 19:02:10 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +6,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.getElementById("titulo").innerHTML = dominio();
   console.log("Hello Rigo from the console!");
+};
+
+const dominio = () => {
+  let newDominio = " ";
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".es"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let o = 0; o < adj.length; o++) {
+      for (let p = 0; p < noun.length; p++) {
+        for (let u = 0; u < ext.length; u++) {
+          newDominio += pronoun[i] + adj[o] + noun[p] + ext[u] + "<br>";
+        }
+      }
+    }
+  }
+  return newDominio;
 };
